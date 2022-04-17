86d08eca39aaf986cca5b9202b1ba7ec11ea36eb
jonathan <camanojhonatan@gmail.com>
2022-04-17 18:01:16 -0500

    se hicieron unas modificaciones

--- usuario-service/src/main/java/com/jonathan/rest/usuarioservice/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/jonathan/rest/usuarioservice/UsuarioServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsuarioServiceApplication {
-
+																																																	
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioServiceApplication.class, args);
 	}
