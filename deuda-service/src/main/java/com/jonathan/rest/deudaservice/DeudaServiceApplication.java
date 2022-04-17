a5bae5ed297999621cdf55209d85551479dbdd3b
jonathan <camanojhonatan@gmail.com>
2022-04-17 18:17:25 -0500

    se creo el proyecto de servicio

---
+++ deuda-service/src/main/java/com/jonathan/rest/deudaservice/DeudaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.rest.deudaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeudaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeudaServiceApplication.class, args);
+    }
+
+}
---
+++ deuda-service/src/test/java/com/jonathan/rest/deudaservice/DeudaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.rest.deudaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeudaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
