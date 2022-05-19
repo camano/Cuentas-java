e686c9e112bed572f124d83322585ad6b403cb02
Jonathan <jonathan.caamano@parquesoftbogota.com>
2022-05-18 20:45:22 -0500

    se cambio

--- usuario-service/src/main/resources/db.sql
+++
@@ -1,9 +0,0 @@
-create database economiadb;
-
-INSERT INTO roles (id,nombre) VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
-
-select * from roles r ;
-
-select * from usuarios_roles ur ;
-
-select * from usuarios u ;
